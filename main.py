da36c8e7462c22f956f54ed0db1be1a84759000b
Samoni George <gsamonin@mail.ru>
2022-10-09 14:13:06 +0300

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+class Perevod:
+    def FutVMetr(self, x):
+        return x * 0.3
+
+#def  AmVSI(x):
+#    print('Перевод из Американской в СИ')
+#    per = int(input('Укажите единицу измерения:\n 1. Дюйм\n'
+#                    ' 2. Фут \n 3. Ярд\n 4. Миля\n'))
+#    if (per == 2):
+#        print('Выбраты футы')
+#        j = float(input())
+#        FutVMetr(j)
+
+ #   print('Выберите тип перевода. \n 1. Перевод из Американской в СИ'
+ #         ' \n 2. Перевод из Старорусской в СИ \n 3. Выйти из системы')
+ #   x = int(input())
+ #   if (x == 1):
+ #       AmVSI(x)
+ #   elif (x == 2):
+ #       AmVSI(x)
+ #   else:
+ #       return 0
+
+
+if __name__ == '__main__':
+    main()
