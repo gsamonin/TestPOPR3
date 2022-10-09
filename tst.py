235ad60ce487f929189d029551174ab25578d845
Samoni George <gsamonin@mail.ru>
2022-10-09 14:16:47 +0300

    добавлен второй метод перевода из дюймов в метры

--- tst.py
+++ tst.py
@@ -1,12 +1,17 @@
 import unittest
 from main import Perevod
 
+
 class TestPerevod(unittest.TestCase):
     def setUp(self):
         self.per = Perevod()
 
     def test_FutVMetr(self):
         self.assertEqual(self.per.FutVMetr(1), 0.3)
+
+    def test_DumVMetr(self):
+        self.assertEqual(self.per.DumVMetr(1), 0.025)
+
 
 if __name__ == "__main__":
     unittest.main()
